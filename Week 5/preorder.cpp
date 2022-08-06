1d2940bd10a7290699df5972594312a542859922
vartika1910 <65497059+vartika1910@users.noreply.github.com>
2022-08-06 21:17:41 +0530

    Create preorder.cpp

---
+++ Week 5/preorder.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<queue>
+using namespace std;
+
+struct node{
+    int data;
+    struct node* left;
+    struct node* right;
+    
+    
+    node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void preorder(node* &root){
+    if(root== NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+
+
+int main()
+{
+    struct node* root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->left->right = new node(5);
+    root->right->left = new node(6);
+    root->right->right = new node(7);
+    preorder(root);
+    return 0;
+}
